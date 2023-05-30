b50a507e78f701eb6c170acb7bfdfe4a62d81d63
“Enter <jadhavmahesh529@gmail.com>
2023-05-30 23:23:13 +0530

    ADD: Refactor code is added

--- Program.cs
+++ Program.cs
@@ -2,83 +2,9 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            {
-                Random random = new Random();
-                int attendance = random.Next(2);
-
-                if (attendance == 1)
-                {
-                    Console.WriteLine("Employee is Present");
-                }
-                else
-                {
-                    Console.WriteLine("Employee is Absent");
-                }
-                int wagePerHour = 20;
-                int fullDayHours = 8;
-
-                int dailyWage = wagePerHour * fullDayHours;
-
-                Console.WriteLine("Daily Employee Wage: $" + dailyWage);
-
-                int partTimeHours = 4;
-
-                int fullTimeWage = wagePerHour * fullDayHours;
-                int partTimeWage = wagePerHour * partTimeHours;
-
-                Console.WriteLine("Full-time Employee Wage: $" + fullTimeWage);
-                Console.WriteLine("Part-time Employee Wage: $" + partTimeWage);
-                int workingDaysPerMonth = 20;
-
-                int monthlyWage = 0;
-
-                for (int day = 1; day <= workingDaysPerMonth; day++)
-                {
-                    switch (day % 3)
-                    {
-                        case 1:
-                            monthlyWage += partTimeWage;
-                            break;
-                        default:
-                            monthlyWage += fullTimeWage;
-                            break;
-                    }
-                }
-
-                Console.WriteLine("Monthly Employee Wage: $" + monthlyWage);
-
-                int workingHoursLimit = 100;
-                int workingDaysLimit = 20;
-
-                int totalWage = 0;
-                int totalWorkingHours = 0;
-                int totalWorkingDays = 0;
-
-                while (totalWorkingHours < workingHoursLimit && totalWorkingDays < workingDaysLimit)
-                {
-                    switch (totalWorkingDays % 3)
-                    {
-                        case 0:
-                            totalWage += wagePerHour * partTimeHours;
-                            totalWorkingHours += partTimeHours;
-                            break;
-                        default:
-                            totalWage += wagePerHour * fullDayHours;
-                            totalWorkingHours += fullDayHours;
-                            break;
-                    }
-
-                    totalWorkingDays++;
-                }
-
-                Console.WriteLine("Total Monthly Employee Wage: $" + totalWage);
-            }
-
-        }
-
+       
         
+           
         
     }
 }
---
+++ Refactor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Refactor
+    {
+        public static int wagePerHour = 20;
+        public static int fullDayHours = 8;
+        public static int partTimeHours = 4;
+        public static int workingDaysPerMonth = 20;
+        public static int workingHoursLimit = 100;
+        public static int workingDaysLimit = 20;
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int attendance = random.Next(2);
+
+            if (attendance == 1)
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+                CalculateDailyWage();
+                CalculateTimeWageHours();
+                CalculateMonthlyWage();
+                CalculateTotalWage();
+            }
+;
+        }
+        public static void CalculateDailyWage()
+        {
+            int dailyWage = wagePerHour * fullDayHours;
+
+            Console.WriteLine("Daily Employee Wage: $" + dailyWage);
+
+        }
+        public static void CalculateTimeWageHours()
+        {
+            int fullTimeWage = wagePerHour * fullDayHours;
+            int partTimeWage = wagePerHour * partTimeHours;
+
+            Console.WriteLine("Full-time Employee Wage: $" + fullTimeWage);
+            Console.WriteLine("Part-time Employee Wage: $" + partTimeWage);
+        }
+        public static void CalculateMonthlyWage()
+        {
+            int monthlyWage = 0;
+            int fullTimeWage = wagePerHour * fullDayHours;
+            int partTimeWage = wagePerHour * partTimeHours;
+            for (int day = 1; day <= workingDaysPerMonth; day++)
+            {
+                switch (day % 3)
+                {
+                    case 0:
+                        monthlyWage += partTimeWage;
+                        break;
+                    default:
+                        monthlyWage += fullTimeWage;
+                        break;
+                }
+            }
+            Console.WriteLine(" Monthly Employee Wage: $" + monthlyWage);
+        }
+        public static void CalculateTotalWage()
+        {
+            int totalWage = 0;
+            int totalWorkingHours = 0;
+            int totalWorkingDays = 0;
+
+            while (totalWorkingHours < workingHoursLimit && totalWorkingDays < workingDaysLimit)
+            {
+                switch (totalWorkingDays % 3)
+                {
+                    case 0:
+                        totalWage += wagePerHour * partTimeHours;
+                        totalWorkingHours += partTimeHours;
+                        break;
+                    default:
+                        totalWage += wagePerHour * fullDayHours;
+                        totalWorkingHours += fullDayHours;
+                        break;
+                }
+
+                totalWorkingDays++;
+
+            }
+            Console.WriteLine("Total Monthly Employee Wage: $" + totalWage);
+
+        }
+    }
+}
